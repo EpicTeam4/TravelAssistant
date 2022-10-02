a8db0055c0c85f9af3ba7ca7849b86a75f8b1493
ilya <seversta1@yandex.ru>
2022-10-02 22:33:49 +0300

    fix navigation, icon, made project working

--- app/src/main/java/com/example/travelassistant/features/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/example/travelassistant/features/favourites/FavouritesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.travelassistant.features.notifications
+package com.example.travelassistant.ui.favourites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavouritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is favourites Fragment"
     }
     val text: LiveData<String> = _text
 }
